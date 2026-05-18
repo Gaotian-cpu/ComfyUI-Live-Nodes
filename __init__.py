089da65fd54857b09b59fc985741bc2802724f83
Alex <56201083@qq.com>
2026-05-19 00:24:46 +0800

    新增文件kv对节点

---
+++ __init__.py
@@ -0,0 +1,15 @@
+from .nodes.request_nodes import FileToKeyValue
+
+
+NODE_CLASS_MAPPINGS = {
+    "FileToKeyValue": FileToKeyValue,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "FileToKeyValue": "File to KeyValue",
+}
+
+# 告诉 ComfyUI 前端资源所在的目录
+WEB_DIRECTORY = "./web"
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
---
+++ nodes/request_nodes.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+与请求有关的节点
+"""
+import folder_paths
+from PIL import Image
+import io
+import os
+import json
+
+
+class FileToKeyValue:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                'key_value': ('KEY_VALUE', {'default': dict()}),
+                "file_path": ("STRING", {'default': ''}),
+                "key_name": ("STRING", {"default": ""}),
+                'fail_on_error': ('BOOLEAN', {'default': False})
+            }
+        }
+
+    RETURN_TYPES = ("KEY_VALUE",)
+    FUNCTION = "convert"
+    CATEGORY = "RequestNode/Utils"
+
+    def convert(self, key_value, file_path, key_name, fail_on_error: bool = False):
+        if not isinstance(key_value, dict):
+            key_value = {}
+
+        if not isinstance(file_path, str) or len(file_path) == 0:
+            if fail_on_error:
+                raise ValueError(u'文件路径为空，无法形成键值对')
+            else:
+                print(u'文件路径为空，无法形成键值对')
+                return key_value
+
+        if not os.path.isfile(file_path):
+            if fail_on_error:
+                raise ValueError(u'文件不存在，无法形成键值对：{}'.format(file_path))
+            else:
+                print(u'文件不存在，无法形成键值对：{}'.format(file_path))
+                return key_value
+
+        if not isinstance(key_name, str) or len(key_name) == 0:
+            err_msg = u'键名为空，无法生成键值对！'
+            if fail_on_error:
+                raise ValueError(err_msg)
+            else:
+                print(err_msg)
+                return key_value
+
+        key_value.update({key_name: open(file_path, 'rb')})
+        # Prepare files as a dictionary where keys are field names and values are file paths
+        # Return a dictionary that can be consumed by the Key/Value node
+        return key_value,
+
+
+
